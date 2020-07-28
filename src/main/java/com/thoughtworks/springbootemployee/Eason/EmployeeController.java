47bcb3a74aee7e8eae64e01beda3d334328f2b69
Eason <chenzscd@163.com>
2020-07-28 19:29:17 +0800

    [Company API][Eason] feature: implement addEmployee API in Employee API

--- src/main/java/com/thoughtworks/springbootemployee/Eason/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/Eason/EmployeeController.java
@@ -1,10 +1,7 @@
 package com.thoughtworks.springbootemployee.Eason;
 
 import com.thoughtworks.springbootemployee.model.Employee;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -42,5 +39,10 @@ public class EmployeeController {
             employees.add(new Employee(i, "male"));
         }
         return employees;
+    }
+
+    @PostMapping
+    public Employee addEmployee(@RequestBody Employee employee) {
+        return new Employee(employee.getId(), employee.getGender());
     }
 }
