52d7e5393ceeee5546e71b8e55d711f89a9d99b4
Eason <chenzscd@163.com>
2020-07-28 19:10:27 +0800

    [Company API][Eason] feature: implement getAllEmployees API in Employee API

---
+++ src/main/java/com/thoughtworks/springbootemployee/Eason/EmployeeController.java
@@ -0,0 +1,22 @@
+package com.thoughtworks.springbootemployee.Eason;
+
+import com.thoughtworks.springbootemployee.model.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee(1));
+        employees.add(new Employee(2));
+        return employees;
+    }
+}
