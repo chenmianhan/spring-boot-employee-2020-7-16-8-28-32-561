13b63097e6ec36c5d4335c505300c343e06b604e
Eason <chenzscd@163.com>
2020-07-28 19:52:56 +0800

    [Company API][Eason] refactor: refactor CompanyController

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -14,8 +14,12 @@ public class CompanyController {
     @GetMapping(params = {"page", "pageSize"})
     public List<Company> getCompaniesByPageAndPageSize(int page, int pageSize) {
         List<Company> companies = new ArrayList<>();
-        for (int i = 0; i < pageSize; i++) {
-            companies.add(new Company(i));
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee(4, "alibaba1", 20, "male", 6000));
+        employees.add(new Employee(11, "tengxun2", 19, "female", 7000));
+        employees.add(new Employee(6, "alibaba3", 19, "male", 8000));
+        for (int i = 0; i < 5; i++) {
+            companies.add(new Company(i, "alibaba", 200, employees));
         }
         return companies;
     }
@@ -23,29 +27,37 @@ public class CompanyController {
     @GetMapping
     public List<Company> getAllCompanies() {
         List<Company> companies = new ArrayList<>();
-        companies.add(new Company(1));
-        companies.add(new Company(2));
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee(4, "alibaba1", 20, "male", 6000));
+        employees.add(new Employee(11, "tengxun2", 19, "female", 7000));
+        employees.add(new Employee(6, "alibaba3", 19, "male", 8000));
+        companies.add(new Company(1, "alibaba", 200, employees));
         return companies;
     }
 
     @GetMapping("/{id}")
     public Company getCompanyById(@PathVariable int id) {
-        return new Company(id);
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee(4, "alibaba1", 20, "male", 6000));
+        employees.add(new Employee(11, "tengxun2", 19, "female", 7000));
+        employees.add(new Employee(6, "alibaba3", 19, "male", 8000));
+        return new Company(id, "alibaba", 200, employees);
     }
 
     @GetMapping("/{id}/employees")
     public List<Employee> getEmployeesByCompanyId(@PathVariable int id) {
-        Company company = new Company(id);
         List<Employee> employees = new ArrayList<>();
-        employees.add(new Employee(1));
-        employees.add(new Employee(2));
+        employees.add(new Employee(4, "alibaba1", 20, "male", 6000));
+        employees.add(new Employee(11, "tengxun2", 19, "female", 7000));
+        employees.add(new Employee(6, "alibaba3", 19, "male", 8000));
+        Company company = new Company(id);
         company.setEmployees(employees);
         return company.getEmployees();
     }
 
     @PostMapping
     public Company addCompany(@RequestBody Company company) {
-        return company;
+        return new Company(company.getId(), company.getCompanyName(), company.getEmployeeNumber(), company.getEmployees());
     }
 
     @PutMapping("/{id}")
@@ -53,6 +65,8 @@ public class CompanyController {
         Company oldCompany = new Company(id);
         oldCompany.setId(newCompany.getId());
         oldCompany.setEmployees(newCompany.getEmployees());
+        oldCompany.setCompanyName(newCompany.getCompanyName());
+        oldCompany.setEmployeeNumber(newCompany.getEmployeeNumber());
         return oldCompany;
     }
 
@@ -60,9 +74,9 @@ public class CompanyController {
     public Company deleteAllEmployeesByCompanyId(@PathVariable int id) {
         Company company = new Company(id);
         List<Employee> employees = new ArrayList<>();
-        employees.add(new Employee(1));
-        employees.add(new Employee(2));
-        employees.add(new Employee(3));
+        employees.add(new Employee(4, "alibaba1", 20, "male", 6000));
+        employees.add(new Employee(11, "tengxun2", 19, "female", 7000));
+        employees.add(new Employee(6, "alibaba3", 19, "male", 8000));
         company.setEmployees(employees);
         company.setEmployees(new ArrayList<>());
         return company;
