32b953724e605a2866bcb8cda0ab6140bfd86cf7
CHENJA28 <CHENJA28@OOCL.COM>
2020-08-02 23:13:04 +0800

    [Company][James] feature:add flyway configuration

---
+++ src/main/resources/db/migration/V1.0__create_employee_table.sql
@@ -0,0 +1,16 @@
+create table company
+(
+id int   AUTO_INCREMENT  PRIMARY KEY,
+company_name varchar(255),
+employee_number int
+);
+create table employee
+(
+id int  AUTO_INCREMENT PRIMARY KEY,
+name varchar(255),
+age int,
+gender varchar(255),
+salary int,
+company_id int,
+foreign key (company_id) references  company (id)
+);
